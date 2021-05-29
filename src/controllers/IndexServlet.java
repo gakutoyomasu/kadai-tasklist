a9caced52679c5147254875aeacb3cad89d8fe2c
gaku.toyomasu <g.toyomasu@gmail.com>
2021-05-30 01:18:02 +0900

    Add database files

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -13,8 +13,8 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,11 +22,12 @@ public class IndexServlet extends HttpServlet {
         super();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
 
 }
